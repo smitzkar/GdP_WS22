aa48e78af4333f373280999eae5f4e60419f7bc9
Karl Erik Schmitz <smitzkar@rudow.informatik.hu-berlin.de>
2022-10-26 13:46:09 +0200

    Finish aufgabe1.txt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
-// Aufgabe 1  
-
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Die Welt ist schön!");
+        System.out.println("Die Welt ist schön!");;
     }
 
 }
