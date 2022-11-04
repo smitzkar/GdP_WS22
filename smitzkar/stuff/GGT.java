d271f558cf541808b48d864f34b50ac1cc353071
smitzkar <karl.e.schmitz@hotmail.com>
2022-11-04 13:38:04 +0100

    Finish GGT

--- smitzkar/stuff/GGT.java
+++ smitzkar/stuff/GGT.java
@@ -1,10 +1,42 @@
-public class Aufgabe2 {
+class GGT {
     public static void main(String[] args) {
-        System.out.println("GdP,\njetzt geht es los :-)");
+        // Read arguments into variables  
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        // Exit on invalid arguments
+        if ((a <= 0) || (b <= 0)) {
+            System.out.println("nur positive ganze Zahlen als Argumente erlaubt");
+            System.exit(-1);
+        }
+        // Optional, check if only two arguments
+        // if (args.length != 2) {
+        //     System.out.println("Only works with two numbers!\nTry again!");
+        //     System.exit(-1);
+        // }
+        // How to check if parseable to integer?  
+
+        // Init variables for euclidian alg
+        int m = a;
+        int n = b;  
+        int r = 1;
+
+        while (r != 0) {
+            // Swap m and n if m < n, using temporary variable
+            if (m < n) {
+                int t;
+                t = m;
+                m = n;
+                n = t;
+            }
+
+            // Steps 3 and 4
+            r = m - n;
+            m = n;
+            n = r;
+        }
+
+
+        System.out.println(m);
     }
 }
-
-20 double  
-25 double  
-25 double  
-25 int  
---
+++ smitzkar/stuff/Summe.java
@@ -0,0 +1,8 @@
+class Summe {
+  public static void main(String args[]) {
+    int x = Integer.parseInt(args[0]);
+    int y = Integer.parseInt(args[1]);
+    int summe = x + y;
+    System.out.println(x + " + " + y + " = " + summe);
+  }
+}
