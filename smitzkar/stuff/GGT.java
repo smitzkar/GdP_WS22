dea2e620af95f5937b5ac42ea1071bfe92708023
smitzkar <karl.e.schmitz@hotmail.com>
2022-11-04 10:46:45 +0100

    Add GGT

---
+++ smitzkar/stuff/GGT.java
@@ -0,0 +1,10 @@
+public class Aufgabe2 {
+    public static void main(String[] args) {
+        System.out.println("GdP,\njetzt geht es los :-)");
+    }
+}
+
+20 double  
+25 double  
+25 double  
+25 int  
