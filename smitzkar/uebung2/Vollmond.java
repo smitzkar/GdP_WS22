4c5a6f822a602f2e680de8303c1c6b4ef2336344
smitzkar <karl.e.schmitz@hotmail.com>
2022-11-15 20:56:57 +0100

    Add pseudocode

---
+++ smitzkar/uebung2/Vollmond.java
@@ -0,0 +1,36 @@
+/* 
+GdP - Uebungsblatt 2 - Aufgabe 1
+Gruppe AG059
+Autoren: ...
+*/ 
+
+// Gesucht: erste n Monate mit 2 Vollmonden (Mondzyklus 29 Tage)
+// TODO  
+
+// main - accepting 1 argument -> int n 
+
+// initialise all needed variables in one place to more easily adapt program later
+
+// procedure to determine if leap year (one solution in lecture notes)
+
+// create array with months: Januar, Februar, Maerz, April, Mai, Juni, Juli, August, September, Oktober, November
+// und Dezember
+// and their corresponding lengths in days 
+// (two dimensional array?)
+
+// outer loop 
+// while (int found != n) ...
+
+// start of new year  
+// if leapyear Februar_length = 29 else Februar_length = 28 
+// year++
+
+// inner loop
+// for (months in monthsArray) ...
+//   // reset moon counter  
+//   moon = 0;
+//   for (int i = 0; i <= currentMonth_length; i++)
+//     if (days % 29 == 1) moon++
+//     if (moon == 2) printline (current year, month), found++ 
+
+
