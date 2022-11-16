6eb44f4ddb18fb14511997e06824d13f3fb41a39
smitzkar <karl.e.schmitz@hotmail.com>
2022-11-16 14:26:12 +0100

    Solve reverse loop

--- smitzkar/praktikum02/Nolindrom.java
+++ smitzkar/praktikum02/Nolindrom.java
@@ -1,37 +1,25 @@
 class Nolindrom {
   public static void main(String[] args) {
     // check if Palindrome  
-    long N = 4321;
+    long N = 40321;
     long R = 0;
     long temp = 0;
-
-
-    // janky workaround  
-    // typecast N longo string, then use .length...
-
-    String stringN = String.valueOf(N);
-    int lengthN = stringN.length();
-    System.out.println(lengthN);
-    
-
 
 
     boolean keepGoing = true;
     while (keepGoing) {
       temp = N % 10;
       N = N / 10;
+      R = R * 10 + temp;
 
-      // doesn't work if there's a 0 somewhere in the number
-      if (temp == 0) {
+      // checks if there's anything left to work on
+      if (N == 0) {
         keepGoing = false;
         continue;
       }
-
-      R = R * 10 + temp;
-
-
-
     }
+
+
 
     System.out.println(R);
 
