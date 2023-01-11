f8c8677bbf398bdaf89760356bdb5a9524c584d2
smitzkar <karl.e.schmitz@hotmail.com>
2023-01-11 14:20:11 +0100

    Init MazeSolver

---
+++ smitzkar/uebung5/MazeSolver.java
@@ -0,0 +1,21 @@
+import gdp.stdlib.StdDraw;
+
+public class MazeSolver {
+
+	public static boolean solve(int[][] maze, int row, int col) {
+		/*
+		Hier ist Platz für Ihre Lösung des Labyrinths :-)
+		*/
+	}
+
+	public static void draw(int[][] maze) {
+		/*
+		Hier ist Platz für Ihre Lösung zum Anzeigen
+		*/
+	}
+	
+	/*
+	Hier können Sie bei Bedarf weitere Methoden oder Konstanten
+	unterbringen ...
+	*/
+}
